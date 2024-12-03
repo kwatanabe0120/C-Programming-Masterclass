af9bdb62254044e1c64e3ba8604721e020a86438
kwatanabe <begingree@gmail.com>
2024-12-03 19:21:42 +0900

    pointer basic

---
+++ section2_pointers_basic/pointerInit.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+
+
+int main()
+{
+	int *p;
+	p = NULL;
+	printf("%p\n", p);
+	// printf("%d\n", *p);
+
+
+	return 0;
+}
---
+++ section2_pointers_basic/printValueAndAddress.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+
+int main()
+{
+	int a = 50, b = 80;
+	printf("The value a = %d\n", a);
+	printf("The address = %p\n", &a);
+
+
+	printf("The value b = %d\n", b);
+	printf("The address = %p\n", &b);
+
+
+
+	return 0;
+}
