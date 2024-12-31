3a00e782b9c66055b45711947101c8f0a691d9be
kwatanabe <begingree@gmail.com>
2024-12-31 14:09:05 +0900

    finished section6

--- section6_array_swapping/arraySwapping.c
+++ section6_array_swapping/arraySwapping.c
@@ -1,4 +1,5 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 #define SIZE 5
 
@@ -22,22 +23,50 @@ void swapArr(int *arr1, int *arr2)
 		swap(&arr1[i], &arr2[i]);
 }
 
+void swapArrBetter(void **arr1, void **arr2)
+{
+	void *temp = *arr1;
+	temp = *arr1;
+	*arr1 = *arr2;
+	*arr2 = temp;
+}
+
+int *generateArray(int size)
+{
+	int i;
+	int *arr;
+	arr = (int*)malloc(size*sizeof(int));
+	for(i = 0; i < size; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+	return arr;
+}
+
 
 int main()
 {
-	int arr1[SIZE] = {1,2,3,4,5};
-	int arr2[SIZE] = {6,7,8,9,10};
+	// int arr1[SIZE] = {1,2,3,4,5};
+	// int arr2[SIZE] = {6,7,8,9,10};
+	int *arr1, *arr2;
+	int sizeArr1, sizeArr2;
+
+	printf("Enter size for Array1: ");
+	scanf("%d", &sizeArr1);
+	printf("Enter size for Array2: ");
+	scanf("%d", &sizeArr2);
+
+	arr1 = generateArray(sizeArr1);
+	arr2 = generateArray(sizeArr2);
 
 	printf("Before swap\n");
 	prinfArr(arr1);
 	prinfArr(arr2);
 	printf("\nAfter swap\n");
-	swapArr(arr1,arr2);
+	// swapArr(arr1,arr2);
+	swapArrBetter(&arr1, &arr2);
 	prinfArr(arr1);
 	prinfArr(arr2);
-
-
-
 
 	return 0;
 }
