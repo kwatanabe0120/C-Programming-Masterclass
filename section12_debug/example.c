db710d403af3535460a2878730fec9cb8b0f51d5
kwatanabe <begingree@gmail.com>
2025-01-14 19:59:05 +0900

    finished section12

---
+++ section12_debug/example.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main()
+{
+	int num1,num2;
+	float average;
+
+	printf("Enter num1: ");
+	scanf("%d", &num1);
+	printf("Enter num2: ");
+	scanf("%d", &num2);
+
+	average  = (float)(num1+num2)/2;
+	printf("The average is %f\n", average);
+
+
+	printf("tets\n\n\n\n");
+	printf("The average is %f\n", average);
+	printf("The average is %f\n", average);
+
+	return 0;
+}
